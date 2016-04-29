b6e8bfafa3353755de3c33ded7c9a7398834ffaa
HellMagic <546cong@gmail.com>
2016-04-29 11:53:25 +0800

    test ten success

---
+++ app/components/dashboard/level-report.jsx
@@ -0,0 +1,47 @@
+import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import classNames from 'classnames/bind';
+import Radium from 'radium';
+const ReactHighcharts = require('react-highcharts');
+
+const config = {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: '分档报告'
+    },
+    xAxis: {
+        categories: ['一档 15%<br /> 大于600分', '二挡 20%<br /> 大于520分','三挡 25%<br /> 大于480分'],
+        title: {
+            text: null
+        }
+    },
+    tooltip: {
+        valueSuffix: '分'
+    },
+    series: [{
+        name: '分数',
+        data: [40, 260, 480]
+    }]
+};
+
+const LevelReport = (data) => {
+    return (
+        <div style={[styles.item, styles.common.radius]}>
+            <ReactHighcharts config={config}></ReactHighcharts>
+        </div>
+    )
+}
+
+export default Radium(LevelReport);
+
+const styles = {
+    common: {
+        radius: {
+            borderRadius: 15
+        }
+    },
+    item: {height: 320, backgroundColor: '#336699', flexGrow: 1, textAlign: 'center', color: '#ffffff', borderRadius: 15, overflow: 'hidder'}
+};
--- app/containers/Dashboard.jsx
+++ app/containers/Dashboard.jsx
@@ -14,6 +14,7 @@ import _ from 'lodash';
 
 import ExamGuideComponent from '../components/dashboard/exam-guide';
 import ScoreRank from '../components/dashboard/score-rank';
+import LevelReport from '../components/dashboard/level-report';
 
 import {initExamGuide} from '../reducers/dashboard/actions';
 
@@ -77,9 +78,7 @@ class Dashboard extends React.Component {
                     </div>
                 </div>
                 <div style={[styles.container, styles.common.radius]}>
-                    <div style={[styles.item, styles.common.radius]}>
-                        <div style={{fontWeight: 'blod', marginTop: 10}}>分档分析报告</div>
-                    </div>
+                    <LevelReport />
                     <div style={[styles.item, styles.common.radius, {marginLeft: 20, marginRight: 20}]}>
                         <div style={{fontWeight: 'blod', marginTop: 10}}>班级分析报告</div>
                     </div>
