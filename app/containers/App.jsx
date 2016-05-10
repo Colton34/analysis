5ca8c5b9d083dc3241a1dd3a8365b70c214cb23d
ganbinjun <ganbinjun@iyunxiao.com>
2016-05-10 19:53:52 +0800

    校级报告header css;  app背景css;

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -36,7 +36,7 @@ class App extends React.Component {
     render() {
         var user = (Map.isMap(this.props.user)) ? this.props.user.toJS() : this.props.user;
         return (
-            <div>
+            <div style={{backgroundColor: '#f2f2f2'}}>
                 <Header user={user} actions={this.props.actions}/>
                 <Dialog />
                     {this.props.children}
