d0908a5fdbe6f92d6a7d3e661409467db9b8ed99
HellMagic <546cong@gmail.com>
2016-05-12 17:09:21 +0800

    删除没用的log

--- server/routes/unless.js
+++ server/routes/unless.js
@@ -2,7 +2,7 @@
 * @Author: HellMagic
 * @Date:   2016-05-05 11:51:30
 * @Last Modified by:   HellMagic
-* @Last Modified time: 2016-05-06 08:24:55
+* @Last Modified time: 2016-05-12 17:08:50
 */
 
 'use strict';
@@ -18,9 +18,6 @@ router.post('/login', auth.authenticate, function(req, res, next) {
     // res.set('authorization', req.user.token);
     res.cookie('authorization', req.user.token);//写入cookie会受cookie时效性影响，所以最好还是返回后写入localStorage--但可能要考虑到降级，或者将此cookie时效性放大，但是对于登出的处理只在客户端
     //删除--但是在服务端就不再有任何验证了。。。
-
-console.log('登录成功，写入token = ', req.user.token);
-
     res.status(200).json(req.user);
 });
 
