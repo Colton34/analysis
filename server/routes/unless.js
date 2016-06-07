c3e11fd68022a7d2897b6e8b009d49a7a1dec260
HellMagic <546cong@gmail.com>
2016-06-07 21:32:40 +0800

    logout done

--- server/routes/unless.js
+++ server/routes/unless.js
@@ -2,13 +2,14 @@
 * @Author: HellMagic
 * @Date:   2016-05-05 11:51:30
 * @Last Modified by:   HellMagic
-* @Last Modified time: 2016-06-03 15:53:37
+* @Last Modified time: 2016-06-07 21:29:13
 */
 
 'use strict';
 
 var router = require('express').Router();
 var auth = require('auth');
+var _ = require('lodash');
 
 router.get('/login', function(req, res, next) {
     res.render('login');
@@ -16,7 +17,10 @@ router.get('/login', function(req, res, next) {
 
 router.post('/login', auth.authenticate, function(req, res, next) {
     // res.set('authorization', req.user.token);
-    res.cookie('authorization', req.user.token, { domain: '.yunxiao.com'});//写入cookie会受cookie时效性影响，所以最好还是返回后写入localStorage--但可能要考虑到降级，或者将此cookie时效性放大，但是对于登出的处理只在客户端
+
+// console.log('req.hostname = ', req.hostname);
+    var options = (_.includes(req.hostname, 'yunxiao')) ? { domain: '.yunxiao.com'} : {};
+    res.cookie('authorization', req.user.token, options);//写入cookie会受cookie时效性影响，所以最好还是返回后写入localStorage--但可能要考虑到降级，或者将此cookie时效性放大，但是对于登出的处理只在客户端
     //删除--但是在服务端就不再有任何验证了。。。
     res.status(200).json(req.user);
 });
