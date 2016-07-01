d630bb7ed01ef2e2ffbd5be22f18bb6934d3926f
ganbinjun <ganbinjun@iyunxiao.com>
2016-07-01 15:51:13 +0800

    login页面的错误提示；

--- public/login/login.js
+++ public/login/login.js
@@ -1,3 +1,4 @@
+var isLoggingIn = false;
 function submit() {
     var value = $("#loginName").val();
     var password = $("#password").val();
@@ -10,13 +11,21 @@ function submit() {
             var res = (request.responseText) ? (JSON.parse(request.responseText)) : {};
             if(res.errorCode == 1 || res.errorCode == 2) {
                 console.log('错误信息：', res.message);
+                if (!$('.err-message-ctn').length) {
+                    $('.login-yunxiao').append('<div class="err-message-ctn"><p class="err-message">' + res.message + '</p></div>')
+                }
+                $('#btn-login').removeClass('btn-disabled').addClass('account-submit');
+                isLoggingIn = false;
             } else {
                 window.location = '/';
+                //$('.err-message-ctn').remove();
+                //$('#btn-login').removeClass('btn-disabled').addClass('account-submit');
             }
         },
         error: function (request, textStatus, errorThrown) {
             console.log('textStatus == ', textStatus);
             console.log('errorThrown == ', errorThrown);
+            $('#btn-login').removeClass('btn-disabled').addClass('account-submit');
             alert('fail');
         }
     });
@@ -25,6 +34,13 @@ function submit() {
 
 $('#btn-login').click(function(event) {
     event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-    submit();
+    if (!isLoggingIn){
+        $('#btn-login').removeClass('account-submit').addClass('btn-disabled');
+        submit();
+        isLoggingIn = true;
+    }
+        
+    
 })
 
+//'<div class="err-message-ctn"><p class="err-message">密码错误</p></div>'
