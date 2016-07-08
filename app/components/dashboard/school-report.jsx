24007132da976ccbd95eb55edc41ea781d6df00b
ganbinjun <ganbinjun@iyunxiao.com>
2016-07-08 12:11:24 +0800

    dashboard: 抽取卡片头做组件，防止highcharts重新渲染显示问题。

--- app/components/dashboard/school-report.jsx
+++ app/components/dashboard/school-report.jsx
@@ -3,14 +3,18 @@ import {Link, browserHistory} from 'react-router';
 import dashboardStyle from './dashboard.css';
 import ReactHighcharts from 'react-highcharts';
 
-
-class SchoolReportCard extends React.Component {
-   constructor(props) {
+/**
+ * grade, examid, 
+ * toViewSchoolAnalysis: 返回校级报告的函数
+ */
+class CardHeader extends React.Component {
+    constructor(props){
         super(props);
         this.state = {
             hoverLink: false
         }
     }
+
     onHeaderMouseEnter() {
         this.setState({
             hoverLink: true
@@ -21,7 +25,27 @@ class SchoolReportCard extends React.Component {
             hoverLink: false
         })
     }
-    toViewSchoolAnalysis() {
+    
+    render() {
+        return (
+            <div onClick={this.props.toViewSchoolAnalysis}
+                onMouseEnter={this.onHeaderMouseEnter.bind(this)}
+                onMouseLeave={this.onHeaderMouseLeave.bind(this)}
+                style={_.assign({}, localStyles.linkHeader, this.state.hoverLink ? { color: '#27aef8' } : { color: '#333' }) }>
+                <span style={{ fontSize: 16, marginRight: 10 }}>校级分析报告</span>
+                <span style={_.assign({}, { float: 'right' }, this.state.hoverLink ? { color: '#27aef8' } : { color: '#bfbfbf' }) }>
+                    <i className='icon-right-open-2'></i>
+                </span>
+            </div>
+        )
+    }
+}
+
+class SchoolReportCard extends React.Component {
+   constructor(props) {
+        super(props);
+   }
+   toViewSchoolAnalysis() {
         var {grade, examid} = this.props;
         var targetUrl = grade ? '/school/report?examid=' + examid + '&grade=' + grade : '/school/report?examid=' + examid;
         browserHistory.push(targetUrl);
@@ -85,15 +109,7 @@ class SchoolReportCard extends React.Component {
         return (
             <div style={{ display: 'inline-block', height: 388, padding: '0 0 0 10px', cursor: 'pointer'}} onClick={this.toViewSchoolAnalysis.bind(this)} className='col-md-6'>
                 <div style={{ width: '100%', height: '100%', backgroundColor: '#fff', borderRadius: 5, padding: '0 30px' }}>
-                    <div  onClick={this.toViewSchoolAnalysis.bind(this)}
-                          onMouseEnter={this.onHeaderMouseEnter.bind(this)}
-                          onMouseLeave={this.onHeaderMouseLeave.bind(this)}
-                          style={_.assign({}, localStyles.linkHeader, _this.state.hoverLink ? {color: '#27aef8'} : {color: '#333'})}>
-                        <span style={{fontSize: 16, marginRight: 10 }}>校级分析报告</span>
-                        <span style={_.assign({},{ float: 'right'}, this.state.hoverLink? {color: '#27aef8'} : {color: '#bfbfbf'})}>
-                            <i className='icon-right-open-2'></i>
-                        </span>
-                    </div>
+                    <CardHeader grade={grade} examid={examid} toViewSchoolAnalysis={this.toViewSchoolAnalysis.bind(this)}/>
                     {/*<div className={dashboardStyle['school-report-img']} style={{marginTop: 30}}></div>*/}
                     <ReactHighcharts config={config} style={{ width: 535, height: 240, marginTop: 30}}></ReactHighcharts>
                 </div>
