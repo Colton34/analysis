98d1bad2d9b12f507ab29e95584ce6af250c0a9c
HellMagic <546cong@gmail.com>
2016-07-19 10:01:09 +0800

    clean state files

--- app/states/global-app-state.js
+++ app/states/global-app-state.js
@@ -2,22 +2,17 @@
 * @Author: HellMagic
 * @Date:   2016-04-11 19:12:00
 * @Last Modified by:   HellMagic
-* @Last Modified time: 2016-05-06 17:51:52
+* @Last Modified time: 2016-07-19 09:58:52
 */
 
 'use strict';
-
-/*
-Header肯定是一直需要user的，而Header是所有的View都需要的，而把user放在global_state里
-
- */
 
 import Immutable, {Record} from 'immutable';
 
 const InitialState = Record({
 
     haveInit: false,
-    user: {},//前端需要知道user的基本信息，而且还需要匹配权限
+    user: {},//Note：前端需要知道user的基本信息，而且还需要匹配权限
     isLoading: false,
     dialog: {
         show: false,
@@ -26,7 +21,7 @@ const InitialState = Record({
         onHide: '',
         shapeStyle:'',
         okButton: true,
-        okLabel: '确认'        
+        okLabel: '确认'
     }
 });
 
