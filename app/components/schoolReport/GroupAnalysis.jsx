950c5ae5fdb3e42492d764c47ae5d0263fc50fc0
DingGaiHua <dinggaihua@iyunxiao.com>
2016-07-27 17:29:27 +0800

    更新颜色列表

--- app/components/schoolReport/GroupAnalysis.jsx
+++ app/components/schoolReport/GroupAnalysis.jsx
@@ -58,16 +58,16 @@ class Dialog extends React.Component {
             return;
         }
 
-        
+
         //levelBuffers的顺序是和levels对应的--显示的时候是倒序
         this.levelBuffers[levBufLastIndex-index] = value;
         //检测如果添加了此buffer，那么保证顺序是对的，由小到大。拿到当前生成的两个值，左边的要比它左边的大，右边的要比它右边的小（前提是如果左右边有值的话）：
         var newSegments = makeCriticalSegments(this.levelBuffers, this.props.levels);
         var invalidIndex = -1;
         var segmentsIsValid = true;
-        
+
         segmentsIsValid = _.every(_.range(newSegments.length-1), (index) => {
-            var valid = newSegments[index+1] > newSegments[index] 
+            var valid = newSegments[index+1] > newSegments[index]
             invalidIndex = valid ? -1 : (levBufLastIndex - parseInt(index/2));
             return valid;
         });
@@ -216,7 +216,7 @@ class GroupAnalysis extends React.Component {
         var xAxis = _.map(levels, (levObj, levelKey) => numberMap[(levelKey-0)+1]+'档');
         var disData = criticalStudentsDiscription(criticalLevelInfo);
 
-        var serisData = {}, colorList=['#00abfe','#00d3c8','#45c44e','#00abfe','#45c44e'];//TODO: 颜色列表
+        var serisData = {}, colorList=['#0099ff','#33cc33','#33cccc','#ff9900','#ff6633','#6666cc'];//TODO: 颜色列表
         _.each(disData, (dataMap, dataKey) => {
             var serisItem = [];
             _.each(dataMap, (dataArr, levelKey) => {
