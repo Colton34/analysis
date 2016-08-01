8ae065a9f57eed6397cc3de4650704f4eb5ee15e
HellMagic <546cong@gmail.com>
2016-08-01 16:20:08 +0800

    添加注释思路

--- app/containers/Test.jsx
+++ app/containers/Test.jsx
@@ -39,7 +39,6 @@ class One extends React.Component {
     render() {
         console.log('one render');
         var one = (Map.isMap(this.props.one)) ? this.props.one.toJS() : this.props.one;
-        // var one = this.props.one;
         return (
             <h1>Hi, {one.a.age}</h1>
         );
@@ -47,6 +46,7 @@ class One extends React.Component {
 }
 
 class Two extends React.Component {
+    //像这种没有通过redux的connect绑定的则需要手动调用shouldComponentUpdate来避免重绘。上面One没有使用，这里使用了，可以通过log看到区别。
     shouldComponentUpdate(nextProps, nextState) {
       console.log('two if equal: ', nextProps.two.equals(this.props.two));
       return (!nextProps.two.equals(this.props.two));
@@ -55,7 +55,6 @@ class Two extends React.Component {
     render() {
         console.log('two render');
         var two= (List.isList(this.props.two)) ? this.props.two.toJS() : this.props.two;
-        // var two = this.props.two;
         return (
             <h1>oh, {two.length}</h1>
         );
@@ -79,10 +78,13 @@ class Test extends React.Component {
     }
 
     render() {
+        //这里完全没必要再判断undefined了--因为reducer那里已经补全--但是如果有两层级那么还是要判断，因为初始化不完备。
         return (
             <div>
                 <div><button onClick={this.props.changeOne}>Click One</button></div>
                 <div><button onClick={this.props.changeTwo}>Click Two</button></div>
+                {/*思路：传递给组件的是immutable对象--为了支持shouldComponentUpdate判断，在这些组件内部再toJS()--方便计算使用。但如果只是纯渲染，那么Immutable本身也有许多util
+                方法来支持，没必要一定转换成plain JS Object*/}
                 <One one={this.props.one} />
                 <Two two={this.props.two} />
             </div>
