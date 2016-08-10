2eec4c4468007650ba46a01aae87ce946dcc832a
HellMagic <546cong@gmail.com>
2016-08-10 16:44:39 +0800

    subject distribution score contriFactor

--- app/components/classReport/singleClassReport/SubjectExamPerformance/subjectDistribution-ScoreContriFactor.jsx
+++ app/components/classReport/singleClassReport/SubjectExamPerformance/subjectDistribution-ScoreContriFactor.jsx
@@ -1,61 +1,27 @@
 //学科得分贡献指数
-
+import _ from 'lodash';
 import React, { PropTypes } from 'react';
-makeFactor
 import {makeFactor} from '../../../../api/exam';
 
-export default function SubjectContriFactor() {
+export default function SubjectContriFactor({reportDS, currentClass}) {
 
 }
 
 //=================================================  分界线  =================================================
 
-function temp() {
-    var subjectMeanInfo = makeClassExamMeanInfo(examStudentsInfo, examPapersInfo, examInfo, examClassesInfo, studentsGroupByClass);
-    var factorsTableData = theClassExamMeanFactorsTable(examInfo, subjectMeanInfo, studentsGroupByClass, headers);
+// export default function SubjectContriFactor({reportDS, currentClass}) {
+//     var examInfo = reportDS.examInfo.toJS(), examStudentsInfo = reportDS.examStudentsInfo.toJS(), examPapersInfo = reportDS.examPapersInfo.toJS(), examClassesInfo = reportDS.examClassesInfo.toJS(), studentsGroupByClass = reportDS.studentsGroupByClass.toJS(), headers = reportDS.headers.toJS();
+//     var theDS = getDS(examInfo, examStudentsInfo, examPapersInfo, examClassesInfo, studentsGroupByClass, headers, currentClass);
+// }
+
+function getDS(examInfo, examStudentsInfo, examPapersInfo, examClassesInfo, studentsGroupByClass, headers, currentClass) {
+    var subjectMeanInfo = makeClassExamMeanInfo(examInfo, examStudentsInfo, examPapersInfo, examClassesInfo, studentsGroupByClass, currentClass);
+    var factorsTableData = theClassExamMeanFactorsTable(subjectMeanInfo, examInfo, studentsGroupByClass, headers, currentClass);
 }
 
-/**
- * TODO：但是当前要的是一个图表
- * //是平均得分率的小数表示的matrix
- * @param  {[type]} subjectMeanInfo [description]
- * @param  {[type]} headers         [description]
- * @return {[type]}                 [description]
- */
-function theClassExamMeanFactorsTable(examInfo, subjectMeanInfo, studentsGroupByClass, headers) {
-    var orderSubjectNames = _.map(headers.slice(1), (obj) => obj.subject);
-
-    var originalMatrix = makeClassExamMeanOriginalMatirx(subjectMeanInfo, headers);
-    var currentClassFactors = makeFactor(originalMatrix)[0];//应该只剩下一行
-
-    //这里对orderSubjectNames进行Map还是为了保证横轴是按照科目的名称进行排序显示的
-    var currentClassSubjectFactors = _.map(orderSubjectNames, (subjectName, index) => {
-        return {subject: subjectName, factor: currentClassFactors[index]} //应该和orderSubjectNames的数目一样多
-    });
-
-    var orderCurrentClassSubjectFactors = _.sortBy(currentClassSubjectFactors, 'factor');
-    var bestSubject = _.last(orderCurrentClassSubjectFactors).subject;
-    var worstSubject = _.first(orderCurrentClassSubjectFactors).subject;
-    return {
-        currentClassSubjectFactors: currentClassSubjectFactors,
-        bestSubject: bestSubject,
-        worstSubject: worstSubject
-    }
-}
-
-function makeClassExamMeanOriginalMatirx(subjectMeanInfo, headers, currentClass) {
-    var matrix = [], subjectMenaObj = subjectMeanInfo[currentClass];
-    var totalSchoolMeanObj = subjectMeanInfo.totalSchool;
-
-    matrix.push(_.map(headers, (headerObj) => totalSchoolMeanObj[headerObj.id].meanRate));
-    matrix.push(_.map(headers, (headerObj) => (subjectMenaObj[headerObj.id]) ? subjectMenaObj[headerObj.id].meanRate : '无数据'));
-
-    return matrix;
-}
-
-
-function makeClassExamMeanInfo(examStudentsInfo, examPapersInfo, examInfo, examClassesInfo, studentsGroupByClass) {
+function makeClassExamMeanInfo(examInfo, examStudentsInfo, examPapersInfo, examClassesInfo, studentsGroupByClass, currentClass) {
     var result = {};
+    result.totalSchool = makeOriginalSubjectInfoRow(examStudentsInfo, examPapersInfo, examInfo, examClassesInfo);
     result[currentClass] = makeOriginalSubjectInfoRow(studentsGroupByClass[currentClass], examPapersInfo, examInfo, examClassesInfo);
     return result;
 }
@@ -82,3 +48,40 @@ function makeOriginalSubjectInfoRow(students, examPapersInfo, examInfo, examClas
     });
     return result;
 }
+
+/**
+ * TODO：但是当前要的是一个图表
+ * //是平均得分率的小数表示的matrix
+ * @param  {[type]} subjectMeanInfo [description]
+ * @param  {[type]} headers         [description]
+ * @return {[type]}                 [description]
+ */
+function theClassExamMeanFactorsTable(subjectMeanInfo, examInfo, studentsGroupByClass, headers, currentClass) {
+    var orderSubjectNames = _.map(headers.slice(1), (obj) => obj.subject);
+
+    var originalMatrix = makeClassExamMeanOriginalMatirx(subjectMeanInfo, headers, currentClass);
+    var currentClassFactors = makeFactor(originalMatrix)[0];//应该只剩下一行
+    //这里对orderSubjectNames进行Map还是为了保证横轴是按照科目的名称进行排序显示的
+    var currentClassSubjectFactors = _.map(orderSubjectNames, (subjectName, index) => {
+        return {subject: subjectName, factor: currentClassFactors[index]} //应该和orderSubjectNames的数目一样多
+    });
+    var orderCurrentClassSubjectFactors = _.sortBy(currentClassSubjectFactors, 'factor');
+    var bestSubject = _.last(orderCurrentClassSubjectFactors).subject;
+    var worstSubject = _.first(orderCurrentClassSubjectFactors).subject;
+    return {
+        currentClassSubjectFactors: currentClassSubjectFactors,
+        bestSubject: bestSubject,
+        worstSubject: worstSubject
+    }
+}
+
+function makeClassExamMeanOriginalMatirx(subjectMeanInfo, headers, currentClass) {
+    var matrix = [], subjectMenaObj = subjectMeanInfo[currentClass];
+    var totalSchoolMeanObj = subjectMeanInfo.totalSchool;
+
+    matrix.push(_.map(headers, (headerObj) => totalSchoolMeanObj[headerObj.id].meanRate));
+    matrix.push(_.map(headers, (headerObj) => (subjectMenaObj[headerObj.id]) ? subjectMenaObj[headerObj.id].meanRate : '无数据'));
+
+    return matrix;
+}
+
