0666490454367bb4655d558229cc9de42e2e05df
HellMagic <546cong@gmail.com>
2016-08-10 10:13:34 +0800

    total-trend

--- app/components/classReport/singleClassReport/totalScore-trend.jsx
+++ app/components/classReport/singleClassReport/totalScore-trend.jsx
@@ -1,9 +1,9 @@
 //总分分布趋势
 
 
+import _ from 'lodash';
 import React, { PropTypes } from 'react';
-import {makeSegments, makeSegmentsCount} from '../../../../api/exam';
-
+import {makeSegments, makeSegmentsCount} from '../../../api/exam';
 
 //Mock的数据放在全局这里
 
@@ -16,10 +16,31 @@ export default function Trend({reportDS, currentClass}) {
 
 
 //=================================================  分界线  =================================================
-//TODO: 计算偏度，得到文案数据！
+//TODO: 计算偏度，得到文案数据！-- 没有了？？？
 
-function makeDS(reportDS, studentsGroupByClass, currentClass) {
-    var segments = makeSegments(examInfo.fullMark);
+
+// export default function Trend({reportDS, currentClass}) {
+//     //Mock的数据不要放在这里！！！
+//     var examInfo = reportDS.examInfo.toJS(), examStudentsInfo=reportDS.examStudentsInfo.toJS(), studentsGroupByClass = reportDS.studentsGroupByClass.toJS();
+//     var headerData = getHeaderData(examStudentsInfo);
+//     var chartDS = getChartDS(examInfo.fullMark, studentsGroupByClass, currentClass);
+//     debugger;
+// }
+
+
+function getHeaderData(examStudentsInfo) {
+    var mean = _.mean(_.map(examStudentsInfo, (obj) => obj.score));
+    var bestScore = _.last(examStudentsInfo).score;
+    var worstScore = _.first(examStudentsInfo).score;
+    return {
+        bestScore: bestScore,
+        worstScore: worstScore,
+        mean: mean
+    }
+}
+
+function getChartDS(fullMark, studentsGroupByClass, currentClass) {
+    var segments = makeSegments(fullMark);
 
     var xAxons = _.slice(segments, 1);
     var yAxons = makeSegmentsCount(studentsGroupByClass[currentClass], segments);
