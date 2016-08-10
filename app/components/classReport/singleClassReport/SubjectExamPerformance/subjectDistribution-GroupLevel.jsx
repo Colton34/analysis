dd53002a3c1024a5751f71206356ad386c917740
HellMagic <546cong@gmail.com>
2016-08-10 17:41:33 +0800

    subject distribution group level

--- app/components/classReport/singleClassReport/SubjectExamPerformance/subjectDistribution-GroupLevel.jsx
+++ app/components/classReport/singleClassReport/SubjectExamPerformance/subjectDistribution-GroupLevel.jsx
@@ -1,16 +1,24 @@
 //学科成绩等级的人数分布比例
-
+import _ from 'lodash';
 import React, { PropTypes } from 'react';
 
-export default function SubjectScoreLevelDistribution() {
+import {makeSegmentsCount} from '../../../../api/exam';
+import {LETTER_MAP as letterMap} from '../../../../lib/constants';
+
+export default function SubjectScoreLevelDistribution({reportDS, currentClass}) {
 
 }
 
 
 //=================================================  分界线  =================================================
 
+// export default function SubjectScoreLevelDistribution({reportDS, currentClass}) {
+//     var examPapersInfo = reportDS.examPapersInfo.toJS(), allStudentsPaperMap = reportDS.allStudentsPaperMap.toJS(), headers = reportDS.headers.toJS();
+//     var tableDS = getTableDS(examPapersInfo, allStudentsPaperMap, headers, currentClass);
+//     debugger;
+// }
 
-function theSubjectLevelExamTable(examPapersInfo, allStudentsPaperMap, headers, levelPcentages, currentClass) {
+function getTableDS(examPapersInfo, allStudentsPaperMap, headers, currentClass, levelPcentages=[0, 60, 70, 85, 100]) {//Note: 这个等级是固定不变的
     //默认给出n个等次，然后最后添加1--代表满分，就是1档次的区间，这样才能形成对应的n个区间（则有n+1个刻度）
 //segments依然是从小到大，但这里展示的时候是从大到小（高难度档次在前）
     // levelPcentages = levelPcentages ? levelPcentages.push(1) : ;  //五个刻度，四个档次
@@ -29,7 +37,7 @@ function theSubjectLevelExamTable(examPapersInfo, allStudentsPaperMap, headers, 
         var segments = makeSubjectLevelSegments(paperObj.fullMark, levelPcentages);
         //这应该是当前科目的区分段的count--而不是总分（且一定不包含总分）
         //获取此科目下当前班级学生（不再是所有学生）的成绩
-        var currentClassSubjectStudents = _.filter(allStudentsPaperMap[headerObj.id], (obj) => obj.class == currentClass);
+        var currentClassSubjectStudents = _.filter(allStudentsPaperMap[headerObj.id], (obj) => obj['class_name'] == currentClass);
         var result = makeSegmentsCount(currentClassSubjectStudents, segments); //注意：低等次在前
         result = _.reverse(result);//高等次在前
         //不需要再计算百分比了
@@ -42,4 +50,8 @@ function theSubjectLevelExamTable(examPapersInfo, allStudentsPaperMap, headers, 
     });
 
     return matrix;
+}
+
+function makeSubjectLevelSegments(paperFullMark, levelPcentages) {
+    return _.map(levelPcentages, (levelPercentage) => _.round(_.multiply(_.divide(levelPercentage, 100), paperFullMark), 2));
 }
