236a701a31afe4aa41e23c97bcc6dec28fd7ba23
ganbinjun <ganbinjun@iyunxiao.com>
2016-08-26 11:26:00 +0800

    班级报告-学科贡献指数：分析诊断结论在只有一个科目时的显示问题；

--- app/components/classReport/singleClassReport/SubjectExamPerformance/subjectDistribution-ScoreContriFactor.jsx
+++ app/components/classReport/singleClassReport/SubjectExamPerformance/subjectDistribution-ScoreContriFactor.jsx
@@ -107,11 +107,15 @@ export default function SubjectContriFactor({classStudents, classHeadersWithTota
             <div style={{display: 'inline-block', width: 1140, height: 290, position: 'relative'}}>
               <ReactHighcharts config={config} style={{width: '100%', height: '100%'}}></ReactHighcharts>
             </div>
-            {/*-----------------优势、劣势学科 TODO: 如果headerInfo.greater和headerInfo.lesser相同那么就是只有一个学科没有可比性----------------------- */}
             <div className={singleClassReportStyle['analysis-conclusion']}>
                 <div style={{lineHeight: 1.5}}>分析诊断：</div>
-                <div style={{lineHeight: 1.5}}>根据上图各学科的得分率贡献指数的大小，可知本班级<span style={{color: colorsMap.B03, margin: '0 5px'}}>{headerInfo.greater}</span>学科表现对班级总分水平有较大的教学推进作用，
-                  而<span style={{color: colorsMap.B03, margin: '0 5px'}}>{headerInfo.lesser}</span>学科表现对班级总分水平有较大的牵扯现象</div>
+                {
+                    headerInfo.greater !== headerInfo.lesser ?  (
+                        <div style={{lineHeight: 1.5}}>
+                            根据上图各学科的得分率贡献指数的大小，可知本班级<span style={{color: colorsMap.B03, margin: '0 5px'}}>{headerInfo.greater}</span>学科表现对班级总分水平有较大的教学推进作用，
+                            而<span style={{color: colorsMap.B03, margin: '0 5px'}}>{headerInfo.lesser}</span>学科表现对班级总分水平有较大的牵扯现象。
+                        </div> ) : <div style={{lineHeight: 1.5}}>只有一个科目没有可比性。</div>
+                }
             </div>
         </div>
     )
