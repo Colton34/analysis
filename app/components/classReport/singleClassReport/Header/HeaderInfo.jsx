a32188063bfaae8c9d3d659b8aeaf09890729f95
DingGaiHua <dinggaihua@iyunxiao.com>
2016-08-29 14:49:52 +0800

    本班考试人数及缺考人数字体添加颜色

--- app/components/classReport/singleClassReport/Header/HeaderInfo.jsx
+++ app/components/classReport/singleClassReport/Header/HeaderInfo.jsx
@@ -11,7 +11,8 @@ export default function HeaderInfo({examInfo, subjects, currentExamClassInfo}) {
             <div id='header' style={{borderRadius: 2, backgroundColor: '#fff', padding: 30}}>
                 <p>亲爱的班主任，您好</p>
                 <p>
-                    本次考试，全校{examInfo.gradeName}共<span style={{ color: colorsMap.B03 }}>{examInfo.realClasses.length}</span>个班级，<span style={{ color: colorsMap.B03 }}>{examInfo.realStudentsCount}</span>名学生参加考试，缺考<span style={{ color: colorsMap.B03 }}>{examInfo.lostStudentsCount}</span>名。本班级{currentExamClassInfo.realStudentsCount}名学生参加考试，缺考{currentExamClassInfo.lostStudentsCount}名。
+                    本次考试，全校{examInfo.gradeName}共<span style={{ color: colorsMap.B03 }}>{examInfo.realClasses.length}</span>个班级，<span style={{ color: colorsMap.B03 }}>{examInfo.realStudentsCount}</span>名学生参加考试，缺考<span style={{ color: colorsMap.B03 }}>{examInfo.lostStudentsCount}</span>名。
+                    本班级<span style={{ color: colorsMap.B03 }}>{currentExamClassInfo.realStudentsCount}</span>名学生参加考试，缺考<span style={{ color: colorsMap.B03 }}>{currentExamClassInfo.lostStudentsCount}</span>名。
                     考试学科：<span style={{ color: colorsMap.B03 }}> {_.join(subjects, '、') }</span>，{subjects.length}个学科。
                 </p>
                 <p style={{ marginBottom: 0 }}>
