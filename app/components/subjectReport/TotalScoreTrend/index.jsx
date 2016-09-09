a375475bbd41555da61e486f624a6c1076cad623
DingGaiHua <dinggaihua@iyunxiao.com>
2016-09-09 20:25:32 +0800

    考试基本情况总结修改

--- app/components/subjectReport/TotalScoreTrend/index.jsx
+++ app/components/subjectReport/TotalScoreTrend/index.jsx
@@ -31,7 +31,7 @@ export default function Trend({reportDS, currentSubject}) {
             <TrendChart currentPaperInfo={currentPaperInfo} currentPaperStudentsInfo={currentPaperStudentsInfo}></TrendChart>
             <div className={subjectReportStyle['analysis-conclusion']}>
                 <div>分析诊断：</div>
-                <div>{currentPaperInfo.subject}{summaryInfo}</div>
+                <div>{summaryInfo}</div>
             </div>
             <AnalysisFactor currentPaperInfo={currentPaperInfo} currentPaperStudentsInfo={currentPaperStudentsInfo} reportDS={reportDS} currentSubject={currentSubject} />
         </div>
@@ -47,17 +47,15 @@ export default function Trend({reportDS, currentSubject}) {
 //Note: 这里是classStudents，那么需要currentPaperStudentsInfo
 function getSummaryInfo(currentPaperStudentsInfo) {
     var skewness = _.round(StatisticalLib.sampleSkewness(_.map(currentPaperStudentsInfo, (obj) => obj.score)), 2);
-    if(skewness < 0.5) {
-        return '学科试卷难度过大，学科要求过高，对中低端学生给予的展现空间不足。';
-    } else if(skewness < 0.6) {
-        return '学科试卷整体要求较高，试卷难度较难。';
-    } else if(skewness < 0.7) {
-        return '学科试卷要求有点偏高，试卷难度偏难。';
-    } else if(skewness < 0.8) {
-        return '学科试卷比较适应学生群体水平，考试要求属正常范围。';
-    } else if(skewness < 0.9){
-        return '学科要求偏宽，试卷整体难度偏容易。';
-    }else
-        return '学科试卷难度过容易，不利于中高端学生的学业水平展现。';
+    if(skewness < -0.3) {
+        return '以全年级学科平均分来衡量，这次考试本年级高于学科平均分的学生人数较多，相应高分段的学生人数密度来的较大，而低分段学生的成绩拉扯全年级本学科平均分较为显著，'
+        '请学科组长与任课老师多关注低端的学生。鼓励他们提高总分水平，极有利于提高本班总平均分水平。';
+    } else if(skewness < -0.5) {
+        return '以全年级学科平均分来衡量，这次考试全年级高于学科平均分的学生人数稍多一点，相应高分段的学生密度还是大一些。低分段学生的成绩对全年级学科分有一定的影响，鼓励他们提高总分水平，有利于提高全年级的本学科总平均水平。';
+    } else if(skewness < 0.05) {
+        return '以全年级本学科平均分来衡量，这次考试本班处于平均分两边的学生人数基本相当。总分分布比较对称。';
+    } else if(skewness < 0.3) {
+        return '以全年级学科平均分来衡量，这次考试全年级高于平均分的学生人数比平均分以下学生人数稍少一点，相应低分段学生人数密度稍大一些。但是高分度学生的总分水平比较给力，他们对全年级学科平均分的提高有较大的贡献。';
+    } else  return '以全年级总分平均分来衡量，这次考试全年级低于平均分的学生人数较多，相应高分段学生人数密度来的较小。但是高分度学生的总分水平很给力，他们对全年级学科平均分的保持较高有极大的贡献。';
 
 }
