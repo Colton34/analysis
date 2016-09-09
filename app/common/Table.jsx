a550d7adb4dd79aef3665be2ae3eade64f826f63
ganbinjun <ganbinjun@iyunxiao.com>
2016-09-09 16:17:20 +0800

    Table组件：使用Tip组件不换行的问题；

--- app/common/Table.jsx
+++ app/common/Table.jsx
@@ -3,48 +3,12 @@ import styles from './common.css';
 import _ from 'lodash';
 import {Table as BootTable, Popover} from 'react-bootstrap';
 import {COLORS_MAP as colorsMap} from '../lib/constants';
-
+import Tip from './Tip';
 /**
  * 传入一个tableData 对象,包含表格头数据(ths)和单元格数据(tds)
  * 传入的参数为 tableData
  */
 
-/**
- * props:
- * content: tip显示的内容
- * direction: tip框显示的方向, 待开发
- */
-class Tip extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showTip: false
-        }
-    }
-    onMouseEnter() {
-        this.setState({showTip: true});
-    }
-    onMouseLeave() {
-        this.setState({showTip: false});
-    }
-    render() {
-        return (
-            <div style={{display: 'inline-block'}}>
-                <div style={_.assign({}, { display: 'inline-block', width: 16, height: 16, lineHeight: '16px', borderRadius: '50%', textAlign: 'center', color: '#fff', position: 'relative' }, this.state.showTip ? { backgroundColor: colorsMap.C08 } : {
-                    backgroundColor: colorsMap.C07})}
-                    onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)}>
-                    <i className='icon-help-1'></i>
-                    <p className='tip-block' style={_.assign({},{color: colorsMap.C12, position: 'absolute', top: 26, right: '50%', marginRight: -130}, this.state.showTip ? {display: 'block'} : {display: 'none'})}>
-                    {
-                        this.props.content
-                    }
-                    </p>
-                    
-                </div>
-            </div>
-        )
-    }
-}
 /**
  * props:
  * tableData: 渲染表格的数据
