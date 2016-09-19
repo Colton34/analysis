af59bd29dff35ac20419ab7337b9403a1b9c8360
DingGaiHua <dinggaihua@iyunxiao.com>
2016-09-19 16:14:27 +0800

    添加重点学生信息模块

--- app/components/liankao/multiSchoolReport/index.jsx
+++ app/components/liankao/multiSchoolReport/index.jsx
@@ -2,14 +2,14 @@ import _ from 'lodash';
 import React, { PropTypes } from 'react';
 
 import Header from './Header';
-import Trender from './TotalScoreTrend'
-
+import Trender from './TotalScoreTrend';
+import ImportStudentsModule from './importStudents';
 export default function MultiSchoolReport({reportDS}) {
     return (
         <div>
             <Header examInfo={reportDS.examInfo} />
             <Trender reportDS={reportDS} />
+            <ImportStudentsModule reportDS={reportDS}/>
         </div>
     )
 }
-
