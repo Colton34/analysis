0bc659d60aa7581f1062413d5b522f780c109771
ganbinjun <ganbinjun@iyunxiao.com>
2016-09-22 15:47:01 +0800

    联考报告-设置临界分数：对话框样式；

--- app/components/liankao/multiSchoolReport/CriticalStudent/index.jsx
+++ app/components/liankao/multiSchoolReport/CriticalStudent/index.jsx
@@ -11,12 +11,16 @@ import {formatNewBaseline} from '../../../../sdk';
 import {makeSegmentsCount} from '../../../../api/exam';
 import {updateLevelBuffersAction, saveBaselineAction} from '../../../../reducers/reportDS/actions';
 import commonClass from '../../../../styles/common.css';
-import {DEFAULT_LEVELBUFFER as defaultLevelBuffer, NUMBER_MAP as numberMap, DEFAULT_LEVEL_RADIO_RANGE as defaultRadioRange} from '../../../../lib/constants';
+import {DEFAULT_LEVELBUFFER as defaultLevelBuffer, NUMBER_MAP as numberMap, DEFAULT_LEVEL_RADIO_RANGE as defaultRadioRange, COLORS_MAP as colorsMap} from '../../../../lib/constants';
 
 import TableView from '../../../../common/TableView';
 import EnhanceTable from '../../../../common/EnhanceTable';
 
 var validateRules = [validateIsNumber, validateLevelBuffer];
+
+var localStyle = {
+    btn: {lineHeight: '32px', width: 84, height: 32,  display: 'inline-block',textAlign: 'center',textDecoration: 'none', backgroundColor:'#f2f2f2',color: '#6a6a6a', margin: '0 6px'}
+}
 
 class CriticalInput extends React.Component {
     constructor(props) {
@@ -68,10 +72,11 @@ class CriticalInput extends React.Component {
     render() {
         var className = !(!!this.state.errorMsg) ? 'valid' : 'invalid';
         var levelLastIndex = _.size(this.props.formLevelBufferInfo) - 1;
+        var {levelKey} = this.props;
         return (
-            <div>
+            <div style={{marginBottom: levelKey == 0 ? 0 : 30}}>
                 <div>
-                    <label htmlFor={this.props.id+'-'+'input'}>{numberMap[(levelLastIndex-this.props.levelKey)+1] + '档线'}</label>
+                    <label htmlFor={this.props.id+'-'+'input'}>{numberMap[(levelLastIndex - levelKey)+1] + '档线上下浮动分数'}</label>
                     <input
                       type='text'
                       placeholder={this.props.value}
@@ -79,7 +84,8 @@ class CriticalInput extends React.Component {
                       value={this.props.value}
                       onChange={this.handleChange.bind(this)}
                       onBlur={this.handleBlur.bind(this)}
-                    />
+                      style={{ width: 280, height: 34, display: 'inline-block', textAlign: 'left', paddingLeft: 20, margin: '0 20px'}}
+                    />分
                 </div>
                 {(this.state.errorMsg) ? (<div className={commonClass['validation-error']}>{this.state.errorMsg}</div>) : ''}
             </div>
@@ -96,6 +102,7 @@ class CriticalForm extends React.Component {
             formLevelBufferInfo: _.cloneDeep(propsLevelBuffers),
             childValidState: childValidState
         }
+        this.levels = props.reportDS.levels.toJS();
     }
 
     setFormLevelBufferState(newFormLevelBufferInfo, levelValidSate) {
@@ -135,21 +142,37 @@ class CriticalForm extends React.Component {
     render() {
         var levelLastIndex = _.size(this.state.formLevelBufferInfo) - 1;
         var formIsValid = _.every(this.state.childValidState, (v) => v);
+        var levelSize = _.size(this.levels);
         return (
             <div>
-                <div>设置临界分数线</div>
-                <div>考试成绩分为4档，一档626.5分，二挡577.5分，三挡490.3分，四挡457.1分</div>
-                {
-                    _.map(this.state.formLevelBufferInfo, (levelBuffer, index) => {
-                        return (
-                            <CriticalInput key={index} levelKey={levelLastIndex-index} value={this.state.formLevelBufferInfo[levelLastIndex-index]} reportDS={this.props.reportDS} formLevelBufferInfo={this.state.formLevelBufferInfo} setFormLevelBufferState={this.setFormLevelBufferState.bind(this)} validation={this.validation.bind(this)} />
-                        )
-                    })
-                }
-                <div>
-                    <button onClick={this.handleSubmit.bind(this)} disabled={!formIsValid}>确认</button>
-                    <button onClick={this.handleCancel.bind(this)}>取消</button>
-                </div>
+                <Body style={{ padding: 30 }}>
+                    <div style={{ minHeight: 150 }}>
+                        <div style={{ marginBottom: 20 }}>
+                            考试成绩分为{levelSize}档，
+                            {
+                                _.join(_.range(levelSize).map(num => {
+                                    var index = levelSize - num - 1;
+                                    return numberMap[num + 1] + '档' + this.levels[index].score + '分'
+                                }), ',')
+                            }
+                        </div>
+                        {
+                            _.map(this.state.formLevelBufferInfo, (levelBuffer, index) => {
+                                return (
+                                    <CriticalInput key={index} levelKey={levelLastIndex - index} value={this.state.formLevelBufferInfo[levelLastIndex - index]} reportDS={this.props.reportDS} formLevelBufferInfo={this.state.formLevelBufferInfo} setFormLevelBufferState={this.setFormLevelBufferState.bind(this) } validation={this.validation.bind(this) } />
+                                )
+                            })
+                        }
+                    </div>
+                </Body>
+                <Footer className="text-center" style={{ textAlign: 'center', borderTop: 0, padding: '0 0 30px 0' }}>
+                    <a href="javascript:void(0)" style={_.assign({}, localStyle.btn, { backgroundColor: '#59bde5', color: '#fff' }) } onClick={this.handleSubmit.bind(this) } disabled={!formIsValid}>
+                        确定
+                    </a>
+                    <a href="javascript:void(0)" style={localStyle.btn} onClick={this.handleCancel.bind(this) }>
+                        取消
+                    </a>
+                </Footer>
             </div>
         );
     }
@@ -191,7 +214,10 @@ class CriticalStudentModule extends React.Component {
                     <p>将临近总分各分数线上下的群体视为“临界生”，联考可以给他们多一点关注，找到他们的薄弱点、有针对性促进一下，他们就可能稳定、甚至提升总分档次。这无论是对学生个人，还是对联考整体的教学成就，都有显著的积极作用。</p>
                     <p>系统默认临界分值为 10分，可点击右侧设置按钮进行修改。联考全体临界生群体规模，见下表：</p>
                 </div>
-                <button onClick={this.showModal.bind(this)} style={{position: 'absolute', right: 30, top: 30}}>设置临界分数线</button>
+                <a href="javascript:void(0)" onClick={this.showModal.bind(this)} className={commonClass.button} style={{ width: 120, height: 30, backgroundColor: colorsMap.B03, color: '#fff', position: 'absolute', right: 30, top: 30, borderRadius: 2, lineHeight: '30px' }}>
+                    <i className='icon-cog-2'></i>
+                    设置临界分数
+                </a>
                 <div>
                     <Modal show={ this.state.isDisplay } ref="dialog"  onHide={this.hideModal.bind(this)}>
                         <Header closeButton={false} style={{position: 'relative', textAlign: 'center', height: 60, lineHeight: 2, color: '#333', fontSize: 16, borderBottom: '1px solid #eee'}}>
