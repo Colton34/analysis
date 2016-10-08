0d8503f823e4bf94f3daba534071a3d7498a434f
HellMagic <546cong@gmail.com>
2016-10-08 17:52:32 +0800

    【临时】修改env配置

--- server/config/env.js
+++ server/config/env.js
@@ -2,7 +2,7 @@
 * @Author: liucong
 * @Date:   2016-03-31 11:23:27
 * @Last Modified by:   HellMagic
-* @Last Modified time: 2016-10-07 10:09:10
+* @Last Modified time: 2016-10-08 17:52:15
 */
 
 'use strict';
@@ -24,7 +24,7 @@ var development = {
 };
 
 var test = {
-    "analysisServer"    : "http://ct.yunxiao.com:8158",
+    "analysisServer"    : "http://fx-engine.yunxiao.com",
     "hfsdb": process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://fx:fx123456@server4.yunxiao.com:8300/hfs-test',  //mongodb://localhost:27017/hfs-test
     'fxdb': 'mongodb://fx2:123456@ct.yunxiao.com:19000/fx2',
     "port": 8666,
