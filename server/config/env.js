4f71b4bbf6ec03ec594d55c4c28dee6959dc4786
HellMagic <546cong@gmail.com>
2016-10-31 20:56:28 +0800

    通过环境变量来设置port；重置http port

--- server/config/env.js
+++ server/config/env.js
@@ -2,12 +2,12 @@
 * @Author: liucong
 * @Date:   2016-03-31 11:23:27
 * @Last Modified by:   HellMagic
-* @Last Modified time: 2016-10-31 19:59:01
+* @Last Modified time: 2016-10-31 20:55:09
 */
 
 'use strict';
 
-var development = {
+var production = {
     "analysisServer"    : "http://fx-engine.yunxiao.com", //http://fx-engine.yunxiao.com   http://ct.yunxiao.com:8158
     'fxdb': 'mongodb://fx2:123456@ct.yunxiao.com:19000/fx2',
     "hfsdb": process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://fx:fx123456@server4.yunxiao.com:8300/hfs-test',  //mongodb://localhost:27017/hfs-test
@@ -25,7 +25,7 @@ var test = {
     "analysisServer"    : "http://ct.yunxiao.com:8158",
     'fxdb': 'mongodb://fx2:123456@ct.yunxiao.com:19000/fx2',
     "hfsdb": process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://fx:fx123456@server4.yunxiao.com:8300/hfs-test',  //mongodb://localhost:27017/hfs-test
-    "port": 8667,
+    "port": 8666,
     "client_host": 'localhost',
     'render_host': 'testrender.yunxiao.com',
     "secret": "Ci23fWtahDYE3dfirAHrJhzrUEoslIxqwcDN9VNhRJCWf8Tyc1F1mqYrjGYF",
@@ -36,11 +36,11 @@ var test = {
     "hfsServer": "http://hfs-be.yunxiao.com"
 };
 
-var production = {
+var development = {
     "analysisServer"    : "http://fx-engine.yunxiao.com",
     'fxdb': 'mongodb://hfsfenxi:%5D%237UXrz%5Bjq98@server4-ks.yunxiao.com:8300/hfsfenxi',
     "hfsdb": process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://read:%3A_3IJon%3Cbb08@server4-ks.yunxiao.com:8300/analy4',  //mongodb://localhost:27017/hfs-test
-    "port": 8667,
+    "port": 8666,
     "client_host": 'localhost',
     'render_host': 'testrender.yunxiao.com',
     "secret": "Ci23fWtahDYE3dfirAHrJhzrUEoslIxqwcDN9VNhRJCWf8Tyc1F1mqYrjGYF",
